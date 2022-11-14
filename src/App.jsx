78c75ed0d5bbc7c35690574a5ef6fbb1dbc5ee88
hanamuratukito <74538755+hanamuratukito@users.noreply.github.com>
2022-11-14 16:29:47 +0900

    コンポーネント化

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <h1>こんには！</h1>
+      <p>お元気ですか？</p>
+    </React.Fragment>
+  );
+};
+
+export default App;
